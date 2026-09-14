fde7bdbf7d96f46803b289d5255c2892274bd931
Zihan Chen <zichen686@gmail.com>
2026-09-14 16:14:28 -0400

    Added Main

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -38,7 +38,7 @@ class FizzBuzz {
         }
     }
 
-    private static int getI(int i) {
+    public static int getI(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/lab1/Main.java
@@ -0,0 +1,7 @@
+package lab1;
+
+class Main{
+    public static void main() {
+        FizzBuzz.main(new String[]{"Hi"});
+    }
+}
